5c2f5498eecc4cf0eea7f1d4d382e3e0ca0e04c1
Daniel <daniel1982007@gmail.com>
2020-11-04 00:43:28 +0200

    submit from Daniel

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import EmployeesList from './components/EmployeesList';
 
 class App extends Component {
   render() {
-    const { employees } = this.props;
+    const { employees } = this.props;//because of App has only one prop, so here you don't need with this.props.employees
     return (
       <div className="App">
         <EmployeesList employees={employees} />
--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
 class EmployeesList extends React.Component {
+  constructor(props) {
+    super(props);//props are from parents
+    this.state = {
+      employees: this.props.employees
+    }
+    this.handleChange = this.handleChange.bind(this)
+    
+  }
+
+  handleChange(e) {
+    //return new employees
+    const input = e.target
+    const arr = this.props.employees.filter(item => item.name.toUpperCase().includes(input.value.toUpperCase()))
+    
+    if(!e.target.value) {
+      this.setState({employees: this.props.employees})
+    }
+    else {
+      this.setState({employees: arr})
+    }
+  }
 
   render() {
-    const { employees } = this.props;
+    //const { employees } = this.state.employees;
+    //useState: 
+    //setState
+    //input onChange={}, filter() get a result array, and set this result array to state
+    //when state changed, return new list. JSX refresh with new state object.
     return (
       <React.Fragment>
         <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
+          <input type="text" className="filter-input" data-testid="filter-input" onChange={this.handleChange} />
         </div>
         <ul className="employees-list">
-          { employees.map(employee => (
+          { this.state.employees.map(employee => (
             <li key={employee.name} data-testid="employee">{employee.name}</li>
           ))}
         </ul>
