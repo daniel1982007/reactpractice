b1f13cfcca7e897364b9b94863dae861f04fe44c
Daniel <daniel1982007@gmail.com>
2020-11-04 20:38:59 +0200

    from me

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 class Translator extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      word: ''
+    }
+    //some eventhandle func
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  //event handle func goes from here
+  handleChange(ev) {
+    let input = ev.target
+    //you can't use loop for this, because, each time you press a key into input 
+    //it loops through map obj, if first element check true, next element checks,
+    //maybe it will be false
+    this.setState({word: input.value})
+  }
 
   render() {
     return (
@@ -8,11 +25,23 @@ class Translator extends React.Component {
         <div className="controls">
           <div className="input-container">
             <span>input:</span>
-            <input type="text" className="text-input" data-testid="text-input" />
+            <input 
+              type="text" 
+              className="text-input" 
+              data-testid="text-input" 
+              onChange={this.handleChange}
+            />
           </div>
+
           <div className="input-container">
             <span>output:</span>
-            <input type="text" className="text-output" data-testid="text-output" readOnly />
+            <input 
+              type="text" 
+              className="text-output" 
+              data-testid="text-output" 
+              readOnly 
+              value={this.props.translations.get(this.state.word) || ''} //map usage, obj.get(key)
+            />
           </div>
         </div>
       </React.Fragment>
